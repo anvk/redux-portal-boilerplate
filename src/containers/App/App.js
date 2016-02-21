47db1f78ebebe3e1f8903ed627ffd4feff8dfea6
Alexey Novak <alexey.novak.mail@gmail.com>
2016-02-21 11:43:52 -0500

    improvement: fixing webpack dev server to serve path and to include source maps

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,3 @@
-require('../../../node_modules/toastr/build/toastr.css');
 require('../../../node_modules/bootstrap/dist/css/bootstrap.min.css');
 require('../../../node_modules/bootstrap/dist/js/bootstrap.min.js');
 
